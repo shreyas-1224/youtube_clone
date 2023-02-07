bf4e9d744d3e8062cbcfaba7d53d7ed3d98f7160
shreyas <pshreyas1224@gmail.com>
2023-02-07 19:59:37 +0530

    starter files and dependancies

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+export default function App(){
+    return(
+        <h1>Hey God, I am cloning you tube app</h1>
+    );
+}
