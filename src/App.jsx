39e0c387d92eabf8db76cf3b1492e54f7a457bd8
shreyas <pshreyas1224@gmail.com>
2023-02-09 12:51:58 +0530

    mostly design part

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
-import { BrowserRouter , Routes , Route } from "react-router-dom";
-import { Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
 
-export default function App(){
-    return(
-        <h1>Hey God, I am cloning you tube app</h1>
-    );
-}
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
